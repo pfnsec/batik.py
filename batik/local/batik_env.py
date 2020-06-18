6ebc9e06abb759324f8764bcc30b78e4fff122e1
Peter Sherman <pfnsec@gmail.com>
2020-06-18 16:37:48 +0100

    Fix Env Parser

--- batik/local/batik_env.py
+++ batik/local/batik_env.py
@@ -1,14 +1,13 @@
 import yaml
 import os
 
+env = {}
+
 if(os.path.isfile("./batik.env.yaml")):
     with open("./batik.env.yaml", 'r') as stream:
         try:
             env = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
-
-if env is None:
-    env = {}
 
 get = env.get
