f3664205e44b22ee62ac27a6dcf43995722d534f
Peter Sherman <pfnsec@gmail.com>
2020-07-10 15:39:27 +0100

    Load urls and token from batik.env.yaml

--- batik/remote/base.py
+++ batik/remote/base.py
@@ -3,11 +3,12 @@ import batik.remote.user as user
 import batik.local.batik_env as batik_env
 import os 
 
-URL = os.environ.get('BATIK_BASE_URL') or "http://localhost:4210"
-HUB_URL = os.environ.get('BATIK_HUB_URL') or "http://localhost:4220"
+#URL = os.environ.get('BATIK_BASE_URL') or "https://localhost:4210"
+
+URL = batik_env.get('cluster_url') or "http://localhost:4210"
+HUB_URL = batik_env.get('hub_url') or "https://hub.batik.sh"
 
 def get_auth_token():
-    return batik_env.get("token")
 
     if not batik_env.get("token"):
         print(f"Logging into {HUB_URL}")
@@ -17,3 +18,5 @@ def get_auth_token():
         with user.session() as session:
             res = user.login(session, email, password)
             print("login", res)
+    else:
+        return batik_env.get("token")
