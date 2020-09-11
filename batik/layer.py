9f4b08e11fe482fa3292f6fd6a8403c1d926f913
Peter Sherman <pfnsec@gmail.com>
2020-09-11 01:33:45 +0100

    Wrap layer run() to ensure we fetch ActorFuture results

--- batik/layer.py
+++ batik/layer.py
@@ -1,9 +1,26 @@
+from dask.distributed import ActorFuture
+import logging
 
 
 class Layer:
-    def __init__(self, client=None, **kwargs): 
-        super().__init__(**kwargs)
+    def __init__(self, layer_args={}, **kwargs): 
+        self.layer_args = layer_args
+    def _run(self, payload):
+        if isinstance(payload, ActorFuture):
+            return self.run(payload.result())
+        else:
+            return self.run(payload)
+
+
 
 class Actor:
-    def __init__(self, **kwargs): 
-        super().__init__(**kwargs)
+    def __init__(self, layer_args={}, **kwargs): 
+        self.layer_args = layer_args
+    def _run(self, payload):
+        try: 
+            if isinstance(payload, ActorFuture):
+                return self.run(payload.result())
+            else:
+                return self.run(payload)
+        except Exception as e:
+            logging.exception('')
