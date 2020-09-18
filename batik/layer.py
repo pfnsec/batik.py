42bff902e62129b72b8584391cf46d39842deb09
Peter Sherman <pfnsec@gmail.com>
2020-09-18 03:03:20 +0100

    Pass layer results properly!

--- batik/layer.py
+++ batik/layer.py
@@ -1,15 +1,20 @@
 from dask.distributed import ActorFuture
 import logging
+import os
 
+# TODO: Log results to rabbitmq if present!
 
 class Layer:
     def __init__(self, layer_args={}, **kwargs): 
         self.layer_args = layer_args
     def _run(self, payload):
         if isinstance(payload, ActorFuture):
-            return self.run(payload.result())
+            res = self.run(payload.result())
         else:
-            return self.run(payload)
+            res = self.run(payload)
+        
+        return res
+        
 
 
 
